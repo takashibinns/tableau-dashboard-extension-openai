5215a256d53dd5897fc0d85591d4ff7652413c89
Takashi Binns <takashi.binns@icloud.com>
2023-02-28 11:28:40 -0500

    initial code cleanup

--- tabgpt/app.js
+++ tabgpt/app.js
@@ -1,33 +1,30 @@
+//  Dependencies
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var chatGPTRouter = require('./routes/chatGPT')
-
 const bodyParser = require('body-parser')
 
-
+//  Init express app
 var app = express();
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(logger('dev'));
+
+//  Serve static content (frontend)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+//  Define the rest api routes
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var chatGPTRouter = require('./routes/chatGPT')
 app.use('/chatGPT',chatGPTRouter);
 
 // catch 404 and forward to error handler
@@ -46,4 +43,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//  Export the express app
 module.exports = app;
--- tabgpt/routes/chatGPT.js
+++ tabgpt/routes/chatGPT.js
@@ -1,52 +1,41 @@
+//  Dependencies
 var express = require('express');
 var router = express.Router();
-
 const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
-console.log('loggin process env', process.env.OPENAI_API_KEY)
 
+//  Load environment variables
+require('dotenv').config()
+
+//  Init OpenAI object
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
-  });
-  const openai = new OpenAIApi(configuration);
-
-  //let defaultPrompt ="i want you to be an analyst and look at this data set to help me understand how do i improve my business. data set is - "; 
-  
-  let defaultPrompt ="i want you to be an analyst and look at this data set to help me understand areas which need improvement. Also suggest to me quick win strategies for those areas. data set is - "; 
-
-  async function runChatGPT (promptText) {
-   const completion = await openai.createCompletion({
-     model: "text-davinci-003","temperature": 0.9,
-     prompt: promptText,
-     max_tokens: 500
-   },(err)=>{console.log("Error calling openai service "+ err)});
-
-console.log( "openai completion res" + completion.data.choices[0].text); 
-return completion.data.choices[0].text;
-
-   }
+});
+const openai = new OpenAIApi(configuration);
 
 /* call chatGPT and send response back. */
 router.post('/', function(req, res, next) {
     
-
-    let promptText = req.body.prompt;
-    console.log("Data Received from chat post request--" + JSON.stringify(promptText))
-    
-
-    openai.createCompletion({
-        model: "text-davinci-003","temperature": 0.9,
-        prompt: promptText,
-        max_tokens: 500
-      }).then ( (gptRes)=>{
-        console.log("res from the open ai api call--", gptRes)
-        res.send(gptRes.data.choices[0].text);
-      }, (err)=>{console.log("Error calling chatGPT API service!! error is " +  err);
-      res.send("Error calling chatGPT API service!! error code is --" + err) })
-
-
-
-
+  //  Get the prompt from our API call
+  let promptText = req.body.prompt;
+  console.log("Data Received from chat post request--" + JSON.stringify(promptText))
+  
+  //  Execute API call to OpenAI
+  openai.createCompletion({
+      model: "text-davinci-003",
+      temperature: 0.9,
+      prompt: promptText,
+      max_tokens: 500
+    }).then ( (gptRes)=>{
+      //  Succesful response from OpenAI
+      console.log("res from the open ai api call--", gptRes.data)
+      //  Return the first choice's text
+      res.send(gptRes.data.choices[0].text);
+    }, (err)=>{
+      //  Error from OpenAI API
+      console.log("Error calling chatGPT API service!! error is " +  err);
+      //  Respond with the error message
+      res.send("Error calling chatGPT API service!! error code is --" + err) 
+    })
 });
 
 
